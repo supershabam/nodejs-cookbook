bef79c0277f9774cc4e4bf5db051d8ee2ed1153d
Ian Hansen <ian@supershabam.com>
2013-08-02 01:00:23 -0600

    supported os, description

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,13 @@
-name             'nodejs-cookbook'
+name             'nodejs'
 maintainer       'Ian Hansen'
 maintainer_email 'ian supershabam com'
 license          'MIT'
-description      'Installs/Configures nodejs-cookbook'
+description      'Installs nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe "nodejs", "Installs nodejs from npmjs.org binary tarball"
+
+%w{ debian ubuntu centos redhat }.each do |os|
+  supports os
+end
