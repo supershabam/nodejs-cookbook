0241e43315d85b806a93a0eac2cc909475ba3702
Ian Hansen <ian@supershabam.com>
2013-08-01 18:54:56 -0600

    berks init

---
+++ README.md
@@ -0,0 +1,13 @@
+# nodejs-cookbook cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Ian Hansen (<ian@supershabam.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'nodejs-cookbook'
+maintainer       'Ian Hansen'
+maintainer_email 'ian@supershabam.com'
+license          'MIT'
+description      'Installs/Configures nodejs-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
